a5ab46b2cf03f16730aefa42281dc1f3c2fff2d4
mbrie041 <mbrie041@gmail.com>
2021-09-26 14:37:49 -0400

    Fix camelCase so it handles lowercase and uppercase words properly.

--- exercises/kata9.js
+++ exercises/kata9.js
@@ -4,30 +4,33 @@ const camelCase = function (input) {
   let slicedArray = newArray.slice(1);
   //Isolate the first word
   let firstWord = newArray.slice(0, 1);
-  let returnedValue = [];
+  let pushedValue = [];
+  let newFirstWord = [];
+  for (let index of firstWord) {
+    for (let i = 0; i < index.length; i++) {
+      newFirstWord.push(index[i].toLowerCase());
+    }
+  }
+
   //Convert the first letter of each following word into a capital
   for (let index of slicedArray) {
     for (let i = 0; i < index.length; i++) {
       if (i === 0) {
-        returnedValue.push(index[i].toUpperCase());
+        pushedValue.push(index[i].toUpperCase());
       } else {
-        returnedValue.push(index[i]);
+        pushedValue.push(index[i].toLowerCase());
       }
     }
   }
   //Add each following word to the first word
-  let combined = firstWord + returnedValue;
+  let combined = newFirstWord + pushedValue;
   let combinedWithoutCommas = combined.replace(/,/g, "");
 
   //Return the combined words
   return combinedWithoutCommas.toString();
 };
 module.exports = { camelCase };
-// Input
+
 console.log(camelCase("this is a string"));
-// console.log(camelCase("loopy lighthouse"));
-// console.log(camelCase("supercalifragalisticexpialidocious"));
-// Output
-// thisIsAString
-// loopyLighthouse
-// supercalifragalisticexpialidocious
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));

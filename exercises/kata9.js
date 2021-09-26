cf09258f2f0524c0ea7bff4f4338b016c9eb2838
mbrie041 <mbrie041@gmail.com>
2021-09-26 14:19:06 -0400

    Complete logic for camelcase function and write testsmaking sure it returns camelcase strings

--- exercises/kata9.js
+++ exercises/kata9.js
@@ -1,12 +1,26 @@
-const camelCase = function(input) {
+const camelCase = function (input) {
   //Seperate each word into their own indices in an array
   let newArray = input.split(" ");
-  console.log(newArray);
+  let slicedArray = newArray.slice(1);
   //Isolate the first word
+  let firstWord = newArray.slice(0, 1);
+  let returnedValue = [];
   //Convert the first letter of each following word into a capital
+  for (let index of slicedArray) {
+    for (let i = 0; i < index.length; i++) {
+      if (i === 0) {
+        returnedValue.push(index[i].toUpperCase());
+      } else {
+        returnedValue.push(index[i]);
+      }
+    }
+  }
   //Add each following word to the first word
+  let combined = firstWord + returnedValue;
+  let combinedWithoutCommas = combined.replace(/,/g, "");
+
   //Return the combined words
-  return newArray.toString();
+  return combinedWithoutCommas.toString();
 };
 module.exports = { camelCase };
 // Input

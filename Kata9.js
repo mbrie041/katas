43faaaf78bcf91839e54f214df41fd5c2e6e2652
mbrie041 <mbrie041@gmail.com>
2021-09-26 10:29:13 -0400

    Write out psuedo code

---
+++ Kata9.js
@@ -0,0 +1,16 @@
+const camelCase = function (input) {
+  //Seperate each word into their own indices in an array
+  //Isolate the first word
+  //Convert the first letter of each following word into a capital
+  //Add each following word to the first word
+  //return the combined words
+};
+
+// Input
+// console.log(camelCase("this is a string"));
+// console.log(camelCase("loopy lighthouse"));
+// console.log(camelCase("supercalifragalisticexpialidocious"));
+// Output
+// thisIsAString
+// loopyLighthouse
+// supercalifragalisticexpialidocious

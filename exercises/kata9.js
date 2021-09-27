63c1ed73c44d97225435f32477e1515091169c62
mbrie041 <mbrie041@gmail.com>
2021-09-27 10:46:14 -0400

    Refactor to have camelcase function use spread and deconstruction

--- exercises/kata9.js
+++ exercises/kata9.js
@@ -1,30 +1,14 @@
 const camelCase = function (input) {
   //Seperate each word into their own indices in an array
-  let newArray = input.split(" ");
-  let slicedArray = newArray.slice(1);
-  //Isolate the first word
-  let firstWord = newArray.slice(0, 1);
-  let pushedValue = [];
-  let newFirstWord = firstWord.toString().toLowerCase();
+  const [firstWord, ...restWords] = input.split(" ");
+  return firstWord.toLowerCase() + restWords.map(letterChange).join("");
+};
 
-  //Convert the first letter of each following word into a capital
-  for (let index of slicedArray) {
-    for (let i = 0; i < index.length; i++) {
-      if (i === 0) {
-        pushedValue.push(index[i].toUpperCase());
-      } else {
-        pushedValue.push(index[i].toLowerCase());
-      }
-    }
-  }
-  //Add each following word to the first word
-  let combined = newFirstWord + pushedValue.join("");
-
-  //Return the combined words
-  return combined;
+const letterChange = function (word) {
+  const [first, ...rest] = word;
+  return first.toUpperCase() + rest.join("").toLowerCase();
 };
 module.exports = { camelCase };
-
+console.log(camelCase("supercalifragalisticexpialidocious"));
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
-console.log(camelCase("supercalifragalisticexpialidocious"));

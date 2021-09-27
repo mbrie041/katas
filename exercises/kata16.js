8b0ed1ce5c9ca3976a4e7b8d18f08a27687448e3
mbrie041 <mbrie041@gmail.com>
2021-09-27 15:58:32 -0400

    Initial Commit

---
+++ exercises/kata16.js
@@ -0,0 +1,15 @@
+const makeCase = function (input, caseOption) {
+  return "";
+  // Put your solution here
+};
+
+console.log(makeCase("this is a string", "camel"));
+console.log(makeCase("this is a string", "pascal"));
+console.log(makeCase("this is a string", "snake"));
+console.log(makeCase("this is a string", "kebab"));
+console.log(makeCase("this is a string", "title"));
+console.log(makeCase("this is a string", "vowel"));
+console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", ["upper", "snake"]));
+
+module.exports = { makeCase };

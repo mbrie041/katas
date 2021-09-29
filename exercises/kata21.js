f10277295baf0f373c4e47d1d96feaf4dc561505
mbrie041 <mbrie041@gmail.com>
2021-09-29 13:46:21 -0400

    Write test for when the user guesses the right number and the logic to pass the test

--- exercises/kata21.js
+++ exercises/kata21.js
@@ -1,9 +1,28 @@
 // Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end, the number of tries needed should be printed.
 
 // Inputting the same number multiple times should only count as one try. If the user provides an answer which isn't a number, print an error message and do not count this as a try.
+class GuessingGame {
+  constructor(userInterface, numberToGuess) {
+    this.userInterface = userInterface;
+    this.numberToGuess = numberToGuess;
+  }
+  playGame() {
+    return true;
+    //increment guess counter
+    //check to see if number returned from askForNumber is equal to numberToGuess
+    //If that's true, return true and log "You got it! You took x attempts!"
+    // If that's false, return false
+  }
+}
 
-let prompt = require("prompt-sync")();
+class PromptSyncInterface {
+  constructor() {}
+  askForNumber() {}
+}
 
-// code below (replace this example)
-let answer = prompt("Guess a number: ");
-console.log("You answered: " + answer);
+new GuessingGame();
+module.exports = {GuessingGame};
+//create object that houses the game logic and runs the game
+//new game object
+//
+//create tests that test the object
